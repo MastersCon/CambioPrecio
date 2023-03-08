7eec87964151ba8e9017391159cccf83aa017960
Rafael Gomez <rafaelantoniogomezhilario@gmail.com>
2023-03-08 12:08:53 -0400

    finish work
    finish code for work

--- CambioPrecio/src/app/cambio-precios/cambio-precios.component.ts
+++ CambioPrecio/src/app/cambio-precios/cambio-precios.component.ts
@@ -40,28 +40,57 @@ export class CambioPreciosComponent implements OnInit {
     XLSX.writeFile(wb, this.fileName);
 
   }
-    actualizacion(){
-      const element = document.getElementById('Calld');
-      const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
-      const data = XLSX.utils.sheet_to_json(ws, {header:1});
+  actualizacion(){
+    const element = document.getElementById('Calld');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+    const data = XLSX.utils.sheet_to_json(ws, {header:1});
 
-      const newData = data.slice(1).map((row: any) => {
-        const valor = row[1];
-        let valorRedondeado = Math.ceil(valor/5)*5 - (valor % 5 === 1 ? 1 : 0);
-        if (valor % 10 === 9) {
-          valorRedondeado = valor;
+    const newData = data.slice(1).map((row: any) => {
+      const valor = row[1];
+      let valorRedondeado = valor;
+
+      // Redondear cualquier número que termine en 6, 7 u 8 a 9
+      if (valor % 10 >= 6 && valor % 10 <= 8) {
+        valorRedondeado = Math.floor(valor/10)*10 + 9;
+      }
+
+      // Redondear cualquier número que termine en 1, 2, 3 o 4 a 5, pero solo si es mayor a 60
+      else if (valor % 10 >= 1 && valor % 10 <= 4 && valor > 60) {
+        valorRedondeado = Math.floor(valor/10)*10 + 5;
+      }
+
+      // No redondear números que terminen en 9, pero solo si es mayor a 60
+      else if (valor % 10 === 9 && valor > 60) {
+        valorRedondeado = valor;
+      }
+
+      // Redondear cualquier otro número según las condiciones previas
+      else if (valor % 1 !== 0) {
+        valorRedondeado = Math.floor(valor);
+      } else if (valor > 60) {
+        const ultimoDigito = valor % 10;
+        let ajuste = 0;
+        if (ultimoDigito === 6) {
+          ajuste = -1;
+        } else if (ultimoDigito >= 7) {
+          ajuste = 5;
         }
-        return {
-          Code: row[0],
-          Precio: valor,
-          NuevoPrecio: valorRedondeado
-        }
-      });
+        valorRedondeado = Math.ceil(valor/5)*5 - (valor % 5 === 1 ? 1 : 0) + ajuste;
+      }
+      return {
+        Code: row[0],
+        Precio: valor,
+        NuevoPrecio: valorRedondeado
+      }
+    });
 
-      this.ExcelData = newData;
+    this.ExcelData = newData;
 
-      console.log(newData);
-    }
+    console.log(newData);
+  }
+
+
+
   onButtonClick(event: MouseEvent) {
     (event.target as HTMLButtonElement).disabled = true;
   }
